73807945f23178a1b6f45eb3a425c01689b890e7
Christian Helle <christian.helle@outlook.com>
2022-10-08 23:09:15 +0200

    Start work on out-of-process Visual Studio extension that works on project files

---
+++ src/JMeterCodeGen.Core/ProjectFileLocator.cs
@@ -0,0 +1,9 @@
+﻿namespace ChristianHelle.DeveloperTools.CodeGenerators.JMeter.Core;
+
+public class SwaggerFileGenerator
+{
+    public static string BuildSwaggerFile(string projectPath)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/JMeterCodeGen.VSIX/GenerateTestPlan.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+using Microsoft.VisualStudio.Extensibility.Definitions;
+using System.Diagnostics;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.JMeter.VSIX
+{
+    [CommandIcon(KnownMonikers.Extension, IconSettings.IconAndText)]
+    [Command("JMeterCodeGen.VSIX.GenerateTestPlanCommand", "Generate JMeter Test Plan", placement: CommandPlacement.ToolsMenu)]
+    [CommandVisibleWhen(
+        expression: "SolutionLoaded & IsValidFile",
+        termNames: new string[] { "SolutionLoaded", "IsValidFile" },
+        termValues: new string[] { "SolutionState:Exists", "ClientContext:Shell.ActiveSelectionFileName=(.csproj)$" })]
+    internal class GenerateTestPlanCommand : Command
+    {
+        private readonly TraceSource traceSource;
+
+        public GenerateTestPlanCommand(VisualStudioExtensibility extensibility, TraceSource traceSource, string id)
+            : base(extensibility, id)
+        {
+            this.traceSource = Requires.NotNull(traceSource, nameof(traceSource));
+        }
+
+        public override Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
+        {
+            var activeProjectIdentifier = context["Shell.ActiveProjectIdentifier"];
+            var activeSelectionPath = context["Shell.ActiveSelectionPath"];
+            traceSource.TraceInformation($"Hello from another process!");
+            return Task.CompletedTask;
+        }
+    }
+}
