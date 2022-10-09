0d451886630da428386223a16d80b90733231ee6
Christian Helle <christian.helle@outlook.com>
2022-10-09 23:43:15 +0200

    Create VSIX project

--- src/JMeterCodeGen.VSIX/VSPackage.cs
+++ src/JMeterCodeGen.VSIX/VSPackage.cs
@@ -25,6 +25,7 @@ namespace JMeterCodeGen.VSIX
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class VSPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace JMeterCodeGen.VSIX
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await JMeterCodeGen.VSIX.GenerateTestPlansCommand.InitializeAsync(this);
         }
 
         #endregion
