b6a9dee5e73de8caaa64057a713dfaac07feda31
Christian Helle <christian.helle@outlook.com>
2023-01-21 00:52:44 +0100

    Implement SwaggerFileGenerator

--- src/JMeterCodeGen.Core.Tests/SwaggerFileGeneratorTests.cs
+++ src/JMeterCodeGen.Core.Tests/SwaggerFileGeneratorTests.cs
@@ -5,10 +5,18 @@ namespace JMeterCodeGen.Core.Tests;
 public class SwaggerFileGeneratorTests
 {
     [Fact]
-    public void GenerateTest()
+    public void LaunchAndGetSwaggerFile_Returns_NotNull()
     {
+        var folder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(folder);
+
+        var csproj = Path.Combine(folder, "Sample.csproj");
+        File.WriteAllText(csproj, TestCode.CSProj);
+        File.WriteAllText(Path.Combine(folder, "Program.cs"), TestCode.CSharp);
+        File.WriteAllText(Path.Combine(folder, "launchsettings.json"), TestCode.LaunchSettings);
+
         var sut = new SwaggerFileGenerator();
-        var swaggerFile = sut.BuildProject("");
+        var swaggerFile = sut.LaunchAndGetSwaggerFile(csproj);
         Assert.NotNull(swaggerFile);
     }
 }
--- src/JMeterCodeGen.Core/SwaggerFileGenerator.cs
+++ src/JMeterCodeGen.Core/SwaggerFileGenerator.cs
@@ -1,134 +1,29 @@
 ﻿using System.Diagnostics;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Writers;
-using Swashbuckle.AspNetCore.Swagger;
-using System.Reflection;
-using System.Runtime.Loader;
-using System.Xml.Linq;
+using System.Net;
 
 namespace ChristianHelle.DeveloperTools.CodeGenerators.JMeter.Core;
 
 public class SwaggerFileGenerator
 {
-    public string BuildProject(string projectFilepath)
+    public string LaunchAndGetSwaggerFile(string projectFilepath)
     {
-        var process = Process.Start("dotnet build", projectFilepath);
-        process!.WaitForExit();
+        var dotnet = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
+            "dotnet",
+            "dotnet.exe");
         
-        if (process.ExitCode != 0)
+        using var process = new Process();
+        process.OutputDataReceived += (_, args) => Trace.WriteLine(args.Data);
+        process.ErrorDataReceived += (_, args) => Trace.WriteLine(args.Data);
+        process.StartInfo = new ProcessStartInfo
         {
-            throw new Exception("Failed to build project");
-        }
-
-        XNamespace xmlns = "http://schemas.microsoft.com/developer/msbuild/2003";
-        var projDefinition = XDocument.Load(projectFilepath);
- 
-        var assemblyResultsEnumerable = projDefinition
-            .Element(xmlns + "Project")
-            .Elements(xmlns + "PropertyGroup")
-            .Elements(xmlns + "AssemblyName").Nodes<XContainer>();
- 
-        var assemblyResults = new List<XNode>(assemblyResultsEnumerable);
-        if(assemblyResults.Count == 0)
-        {
-            throw new Exception($"The project file isn't correctly structured: [{projectFilepath}]");
-        }
- 
-        string assemblyName = assemblyResults[0].ToString();
-    }
-
-    public string BuildSwaggerFile(string startupAssemblyPath, string swaggerFile = "v1")
-    {
-        // 1) Configure host with provided startupassembly
-        var startupAssembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(
-            Path.Combine(Directory.GetCurrentDirectory(), startupAssemblyPath));
-
-        // 2) Build a service container that's based on the startup assembly
-        var serviceProvider = GetServiceProvider(startupAssembly);
-
-        // 3) Retrieve Swagger via configured provider
-        var swaggerProvider = serviceProvider.GetRequiredService<ISwaggerProvider>();
-        var swagger = swaggerProvider.GetSwagger(swaggerFile);
-
-        // 4) Serialize to specified output location or stdout
-        var outputPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
-
-        using (var streamWriter = File.CreateText(outputPath))
-        {
-            IOpenApiWriter writer = new OpenApiYamlWriter(streamWriter);
-            swagger.SerializeAsV3(writer);
-        }
-
-        var contents = File.ReadAllText(outputPath);
-        File.Delete(outputPath);
-        return contents;
-    }
-
-    private static IServiceProvider GetServiceProvider(Assembly startupAssembly)
-    {
-        if (TryGetCustomHost(startupAssembly, "SwaggerHostFactory", "CreateHost", out IHost host))
-        {
-            return host.Services;
-        }
-
-        if (TryGetCustomHost(startupAssembly, "SwaggerWebHostFactory", "CreateWebHost", out IWebHost webHost))
-        {
-            return webHost.Services;
-        }
-
-        try
-        {
-            return WebHost.CreateDefaultBuilder()
-                .UseStartup(startupAssembly.GetName().Name)
-                .Build()
-                .Services;
-        }
-        catch
-        {
-            var serviceProvider = HostingApplication.GetServiceProvider(startupAssembly);
-
-            if (serviceProvider != null)
-            {
-                return serviceProvider;
-            }
-
-            throw;
-        }
-    }
-
-    private static bool TryGetCustomHost<THost>(
-        Assembly startupAssembly,
-        string factoryClassName,
-        string factoryMethodName,
-        out THost host)
-    {
-        // Scan the assembly for any types that match the provided naming convention
-        var factoryTypes = startupAssembly.DefinedTypes
-            .Where(t => t.Name == factoryClassName)
-            .ToList();
-
-        if (!factoryTypes.Any())
-        {
-            host = default;
-            return false;
-        }
-
-        if (factoryTypes.Count() > 1)
-            throw new InvalidOperationException($"Multiple {factoryClassName} classes detected");
-
-        var factoryMethod = factoryTypes
-            .Single()
-            .GetMethod(factoryMethodName, BindingFlags.Public | BindingFlags.Static);
-
-        if (factoryMethod == null || factoryMethod.ReturnType != typeof(THost))
-            throw new InvalidOperationException(
-                $"{factoryClassName} class detected but does not contain a public static method " +
-                $"called {factoryMethodName} with return type {typeof(THost).Name}");
-
-        host = (THost)factoryMethod.Invoke(null, null);
-        return true;
+            FileName = dotnet,
+            Arguments = $"run --project {projectFilepath} --urls=http://localhost:54321",
+        };
+        process.Start();
+        
+        using var client = new WebClient();
+        var swaggerSpec = client.DownloadString("http://localhost:54321/swagger/v1/swagger.json");
+        return swaggerSpec;
     }
 }
