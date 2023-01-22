becb7bfb6a255befd548321ef0db9fdb159635e5
Christian Helle <christian.helle@outlook.com>
2023-01-22 21:43:34 +0100

    Introduce TestFiles for convenient test arrange

--- src/JMeterCodeGen.Core.Tests/SwaggerFileGeneratorTests.cs
+++ src/JMeterCodeGen.Core.Tests/SwaggerFileGeneratorTests.cs
@@ -7,15 +7,9 @@ public class SwaggerFileGeneratorTests
     [Fact]
     public void LaunchAndGetSwaggerFile_Returns_NotNull()
     {
-        var folder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
-        Directory.CreateDirectory(folder);
-
-        var csproj = Path.Combine(folder, "Sample.csproj");
-        File.WriteAllText(csproj, TestCode.CSProj);
-        File.WriteAllText(Path.Combine(folder, "Program.cs"), TestCode.CSharp);
-
+        var csproj = TestFiles.Create();
         var sut = new SwaggerFileGenerator();
-        var swaggerFile = sut.LaunchAndGetSwaggerFile(csproj);
-        Assert.NotNull(swaggerFile);
+        var swaggerSpec = sut.LaunchAndGetSwaggerFile(csproj);
+        Assert.NotNull(swaggerSpec);
     }
 }
---
+++ src/JMeterCodeGen.Core.Tests/TestFiles.cs
@@ -0,0 +1,16 @@
+﻿namespace JMeterCodeGen.Core.Tests;
+
+public static class TestFiles
+{
+    public static string Create()
+    {
+        var folder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(folder);
+
+        var csproj = Path.Combine(folder, "Sample.csproj");
+        File.WriteAllText(csproj, TestCode.CSProj);
+        File.WriteAllText(Path.Combine(folder, "Program.cs"), TestCode.CSharp);
+
+        return csproj;
+    }
+}
