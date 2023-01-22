bae21f96205c6ebce21855cff86300a35ee919b8
Christian Helle <christian.helle@outlook.com>
2023-01-22 23:22:44 +0100

    Introduce JMeterScriptGenerator

---
+++ src/JMeterCodeGen.Core.Tests/JMeterScriptGeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿using ChristianHelle.DeveloperTools.CodeGenerators.JMeter.Core;
+
+namespace JMeterCodeGen.Core.Tests;
+
+public class JMeterScriptGeneratorTests
+{
+    [Fact]
+    public void Generate()
+    {
+        var csproj = TestFiles.Create();
+        var swaggerSpec = new SwaggerFileGenerator().LaunchAndGetSwaggerFile(csproj);
+
+        var workingFolder = Path.GetDirectoryName(csproj)!;
+        var outputFolder = Path.Combine(workingFolder, "Output");
+        
+        var swaggerFile = Path.Combine(workingFolder, "swagger.json");
+        File.WriteAllText(swaggerFile, swaggerSpec);
+
+        new JMeterScriptGenerator().Generate(swaggerFile, outputFolder);
+        Assert.NotEmpty(Directory.GetFiles(outputFolder));
+    }
+}
---
+++ src/JMeterCodeGen.Core/JMeterScriptGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Rapicgen.Core.Generators;
+using Rapicgen.Core.Generators.OpenApi;
+using Rapicgen.Core.Installer;
+using Rapicgen.Core.Options.General;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.JMeter.Core;
+
+public class JMeterScriptGenerator
+{
+    public void Generate(string swaggerFilePath, string outputDirectory)
+    {
+        var generator = new OpenApiJMeterCodeGenerator(
+            swaggerFilePath,
+            outputDirectory,
+            new DefaultGeneralOptions(),
+            new ProcessLauncher(),
+            new DependencyInstaller(
+                new NpmInstaller(new ProcessLauncher()),
+                new FileDownloader(new WebDownloader())));
+
+        generator.GenerateCode(null);
+    }
+}
