b63fee8596a272881fcc2d0b40d8f2e93fe7372d
Christian Helle <christian.helle@outlook.com>
2023-01-22 21:43:50 +0100

    Randomize the port

--- src/JMeterCodeGen.Core/SwaggerFileGenerator.cs
+++ src/JMeterCodeGen.Core/SwaggerFileGenerator.cs
@@ -11,21 +11,22 @@ public class SwaggerFileGenerator
             Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
             "dotnet",
             "dotnet.exe");
-        
+
+        var port = new Random().Next(50000, 59999);
         using var process = new Process();
         process.OutputDataReceived += (_, args) => Trace.WriteLine(args.Data);
         process.ErrorDataReceived += (_, args) => Trace.WriteLine(args.Data);
         process.StartInfo = new ProcessStartInfo
         {
             FileName = dotnet,
-            Arguments = $"run --project {projectFilepath} --urls=http://localhost:54321",
+            Arguments = $"run --project {projectFilepath} --urls=http://localhost:{port}",
         };
         process.Start();
 
         try
         {
             using var client = new WebClient();
-            var swaggerSpec = client.DownloadString("http://localhost:54321/swagger/v1/swagger.json");
+            var swaggerSpec = client.DownloadString($"http://localhost:{port}/swagger/v1/swagger.json");
             return swaggerSpec;
         }
         finally
