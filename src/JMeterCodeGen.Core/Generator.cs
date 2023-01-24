a16166e0517bd219a7d1b76f324d686eb125bb97
Christian Helle <christian.helle@outlook.com>
2023-01-24 13:58:48 +0100

    Introduce Generator helper

---
+++ src/JMeterCodeGen.Core/Generator.cs
@@ -0,0 +1,19 @@
+﻿namespace ChristianHelle.DeveloperTools.CodeGenerators.JMeter.Core
+{
+    public static class Generator
+    {
+        public static void GenerateFromProject(string projectFullPath)
+        {
+            var csproj = projectFullPath;
+            var swaggerSpec = SwaggerFileGenerator.LaunchAndGetSwaggerFile(csproj);
+
+            var workingFolder = Path.GetDirectoryName(csproj)!;
+            var outputFolder = Path.Combine(workingFolder, "JMeter");
+
+            var swaggerFile = Path.GetRandomFileName();
+            File.WriteAllText(swaggerFile, swaggerSpec);
+
+            JMeterScriptGenerator.Generate(swaggerFile, outputFolder);
+        }
+    }
+}
